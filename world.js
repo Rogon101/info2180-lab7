c01bacd4551360545341c733f42d1648183936d4
Rogerio Jones <rogonjones@gmail.com>
2018-11-19 19:06:55 +0000

    Exercise 3

---
+++ world.js
@@ -0,0 +1,29 @@
+/*global $*/
+window.onload = main;
+
+ //accepts a country and requests data
+function ajax_request(country){
+    let requestObject = new XMLHttpRequest();
+    let url = "/world.php?country="+country;
+    
+    requestObject.onreadystatechange=function(){
+        if(this.readyState == XMLHttpRequest.DONE){
+            if(this.status == 200){
+                $("#result")[0].innerHTML = "<p>"+this.responseText+"<\p>";
+            }else{
+                $("#result")[0].innerHTML = "Inernal Error";
+            }
+        }
+    }
+    
+    requestObject.open("GET", url, true);
+    requestObject.send();
+}
+
+ function main(){
+    var lookup = document.getElementById("lookup");
+    lookup.onclick = function(){
+        ajax_request(document.getElementById("country").value.trim());
+        
+    }
+}
--- world.php
+++ world.php
@@ -1,18 +1,37 @@
 <?php
 
-$host = getenv('IP');
-$username = getenv('C9_USER');
-$password = '';
-$dbname = 'world';
+if($_SERVER["REQUEST_METHOD"] === "GET"){
+    $host = getenv('IP');
+    $username = getenv('C9_USER');
+    $password = '';
+    $dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    if(isset($_GET['country']) && !empty($_GET['country'])){
+        $country = "%".strip_tags($_GET['country'])."%";
+        $countryQuery = $conn->prepare("SELECT * from countries where name LIKE :country");
+        $countryQuery->bindParam(':country', $country, PDO::PARAM_STR);
+        $countryQuery->execute();
+        $result = $countryQuery->fetch(PDO::FETCH_ASSOC);
+        
+        if($result){
+            echo  $result["name"]." is ruled by ".$result["head_of_state"];
+        }else{
+            echo "No Such Country";
+        }
+    
 
-$stmt = $conn->query("SELECT * FROM countries");
+        $stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        echo '<ul>';
+        foreach ($results as $row) {
+             echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        }
+        echo '</ul>';
+    }else{
+         echo 'use get request :p';
+    }
 }
-echo '</ul>';
